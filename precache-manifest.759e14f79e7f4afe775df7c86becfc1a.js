2e60fd6757f4ab70a4b32cb0c17f4d15511cfd84
Alexander Mostovenko <alexmost1989@gmail.com>
2018-11-06 23:00:30 +0200

    Updates

---
+++ precache-manifest.759e14f79e7f4afe775df7c86becfc1a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/CRA-runtime-example/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "8252ce254a830bf97c65",
+    "url": "/CRA-runtime-example/static/js/runtime~main.8252ce25.js"
+  },
+  {
+    "revision": "4fecc8c1a9496a67a9cc",
+    "url": "/CRA-runtime-example/static/js/main.4fecc8c1.chunk.js"
+  },
+  {
+    "revision": "f99ecf0d7da737532b6b",
+    "url": "/CRA-runtime-example/static/js/1.f99ecf0d.chunk.js"
+  },
+  {
+    "revision": "4fecc8c1a9496a67a9cc",
+    "url": "/CRA-runtime-example/static/css/main.c3849439.chunk.css"
+  },
+  {
+    "revision": "bf310d5ecd6eb9296305c18120cf5ff4",
+    "url": "/CRA-runtime-example/index.html"
+  }
+];

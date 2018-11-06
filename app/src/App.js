bf7be65983ff51895b7c3d392e2ad2e4ea781e44
Alexander Mostovenko <alexmost1989@gmail.com>
2018-11-06 23:55:52 +0200

    added ru locale

--- app/src/App.js
+++ app/src/App.js
@@ -15,6 +15,7 @@ const LangSwitcher = () => (
   <div className="Lang-switch">
     <h2>{ t`Switch lang`}</h2>
     <a href='/' onClick={setLocale('uk')}>uk</a>
+    <a href='/' onClick={setLocale('ru')}>ru</a>
     <a href='/' onClick={setLocale('en')}>en</a>
   </div>
 )
